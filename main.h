0269430fbf9fa204cb07b760cd7c619d91dc3585
jmoseka <jamylam3@gmail.com>
2021-10-06 15:55:33 +0000

    added function prototypes

--- main.h
+++ main.h
@@ -2,5 +2,16 @@
 #define PRINT_F
 
 int _printf(const char *format, ...);
+/**
+ * print - creating a struct to store the specifier
+ * and the neccessary function
+ * specifier: the specifier passed
+ * @f: calls the neccessary function needed
+ */
+typedef struct print
+{
+	char *specifier;
+	int (*f)(va_list);
+}print_t;
 
 #endif
